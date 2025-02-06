7dc59934850fa553dc748739d32e2b9718e449bb
JordanS <jordanserafini.74@gmail.com>
2025-02-06 15:05:51 +0100

    telemaintenance

--- app/telemaintenance/page.tsx
+++ app/telemaintenance/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Image from 'next/image'
 import ISLInput from './ISLinput'
 
 function Telemaintenance() {
   return (
     <div className='telemaintenance'>
+      <Image className='img_telemaintenance' src="/assets/staff_sav.webp" alt="telemaintenance" width={350} height={350} />
       <ISLInput />
     </div>
   )
