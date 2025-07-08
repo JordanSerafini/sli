d1420250c6d7d95b8a64dae4184bf315db466076
JordanS <jordanserafini.74@gmail.com>
2025-07-08 15:22:39 +0200

    patch

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,11 @@ import RootLayout from "./RootLayout";
 export const metadata: Metadata = {
   title: "Solution Logique",
   description: "Votre partenaire informatique.",
+  icons: {
+    icon: '/assets/logo.webp',
+    shortcut: '/assets/logo.webp',
+    apple: '/assets/logo.webp',
+  },
 };
 
 export default function Layout({ children }: { children: ReactNode }) {
